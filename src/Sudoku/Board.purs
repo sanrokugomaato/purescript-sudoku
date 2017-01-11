99b4195e0502b8a44f5d340d3a8958a41984b242
Jun <me@noraesae.net>
2017-01-12 01:28:10 +0900

    Some code

--- src/Sudoku/Board.purs
+++ src/Sudoku/Board.purs
@@ -1,37 +1,85 @@
 module Sudoku.Board where
 
 import Prelude
-import Data.Array (filter, foldMap, range, snoc)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Random (RANDOM, randomInt)
+import Data.Array (filter, foldMap, foldRecM, length, range, snoc, updateAt, (!!), (\\))
+import Data.Array.Partial (head)
+import Data.Maybe (Maybe(..), fromJust, isJust)
+import Partial.Unsafe (unsafePartial)
 
-newtype Cell a = Cell { row :: Int, col :: Int, val :: a }
+data Cell = Cell Int (Maybe Int)
 
-cell :: forall a. Int -> Int -> a -> Cell a
-cell row col val = Cell { row, col, val }
+row :: Cell -> Int
+row (Cell i _) = i `div` 9
 
-row :: forall a. Cell a -> Int
-row (Cell x) = x.row
+col :: Cell -> Int
+col (Cell i _) = i `mod` 9
 
-col :: forall a. Cell a -> Int
-col (Cell x) = x.col
+value :: Cell -> Maybe Int
+value (Cell _ x) = x
 
-instance showCell :: (Show a) => Show (Cell a) where
-  show (Cell cell) = show cell.val
+instance showCell :: Show Cell where
+  show (Cell _ (Just x)) = show x
+  show (Cell _ Nothing) = "x"
 
-newtype Board a = Board (Array (Cell a))
+newtype Board = Board (Array Cell)
 
-instance showBoard :: (Show a) => Show (Board a) where
-  show board = foldMap (nthRow board >>> foldMap show >>> (<>) "\n") (range 0 8)
+instance showBoard :: Show Board where
+  show board = foldMap (flip rowAt board >>> foldMap show >>> (<>) "\n") (range 0 8)
 
-createBoard :: forall a. (Board a -> a) -> Board a
-createBoard f = go f 0 0 []
+emptyBoard :: Board
+emptyBoard = Board $ map (\i -> Cell i Nothing) (range 0 80)
+
+rowAt :: Int -> Board -> Array Cell
+rowAt i (Board arr) = filter (row >>> eq i) arr
+
+colAt :: Int -> Board -> Array Cell
+colAt i (Board arr) = filter (col >>> eq i) arr
+
+sectionAt :: Int -> Int -> Board -> Array Cell
+sectionAt secRow secCol (Board arr) = filter f arr
   where
-    go f row 9 arr = go f (row + 1) 0 arr
-    go _ 9 _ arr = Board arr
-    go f row col arr = go f row (col + 1)
-                       (snoc arr (cell row col (f (Board arr))))
+    f cell =
+      let r = row cell
+          c = col cell
+      in
+       r >= secRow * 3 && r < (secRow + 1) * 3 &&
+       c >= secCol * 3 && c < (secCol + 1) * 3
 
-nthRow :: forall a. Board a -> Int -> Array (Cell a)
-nthRow (Board arr) i = filter (row >>> eq i) arr
+valueAt :: Int -> Int -> Board -> Maybe Int
+valueAt row col (Board arr) = arr !! (row * 9 + col) >>= value
 
-nthCol :: forall a. Board a -> Int -> Array (Cell a)
-nthCol (Board arr) i = filter (col >>> eq i) arr
+choose :: Array Int -> Eff (random :: RANDOM) Int
+choose [x] = pure x
+choose xs = do
+  idx <- randomInt 0 (length xs - 1)
+  pure $ unsafePartial fromJust $ xs !! idx
+
+availableValues :: Int -> Int -> Board -> Array Int
+availableValues row col board = range 1 9 \\ already
+  where
+    already = map (unsafePartial fromJust <<< value)
+            $ filter (isJust <<< value)
+            $ rowAt row board <> colAt col board <>
+              sectionAt (row `div` 3) (col `div` 3) board
+
+replaceValue :: Board -> Int -> Int -> Int -> Board
+replaceValue (Board arr) row col val =
+  Board $ unsafePartial fromJust $ updateAt idx (Cell idx $ Just val) arr
+  where idx = row * 9 + col
+
+insertRandomCell :: Board -> Eff (random :: RANDOM) Board
+insertRandomCell board = do
+  row <- randomInt 0 8
+  col <- randomInt 0 8
+  case valueAt row col board of
+    Just _ -> insertRandomCell board -- try again
+    Nothing -> do
+      val <- choose $ availableValues row col board
+      pure $ replaceValue board row col val
+
+defaultBoard :: Eff (random :: RANDOM) Board
+defaultBoard = foldRecM (\board _ -> insertRandomCell board)
+                        emptyBoard
+                        (range 1 17)
