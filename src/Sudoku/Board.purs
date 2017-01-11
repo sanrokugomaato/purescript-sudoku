f537f5a305eeb332435fe08d2bd37b1ffa52406f
Jun <me@noraesae.net>
2017-01-11 22:45:21 +0900

    Remove test
    It's temporary! Run run run away...

--- src/Sudoku/Board.purs
+++ src/Sudoku/Board.purs
@@ -2,9 +2,6 @@ module Sudoku.Board where
 
 import Prelude
 import Data.Array (snoc, filter)
-import Data.Generic.Rep (class Generic)
-import Data.Generic.Rep.Eq (genericEq)
-import Data.Generic.Rep.Show (genericShow)
 
 newtype Cell a = Cell { row :: Int, col :: Int, val :: a }
 
@@ -17,18 +14,7 @@ row (Cell x) = x.row
 col :: forall a. Cell a -> Int
 col (Cell x) = x.col
 
-derive instance genericCell :: Generic (Cell a) _
-
-instance showCell :: Show a => Show (Cell a) where show = genericShow
-instance eqCell :: Eq a => Eq (Cell a) where eq = genericEq
-
 newtype Board a = Board (Array (Cell a))
-
-derive instance genericBoard :: Generic (Board a) _
-
-instance showBoard :: Show a => Show (Board a) where show = genericShow
-
-instance eqBoard :: Eq a => Eq (Board a) where eq = genericEq
 
 createBoard :: forall a. (Board a -> a) -> Board a
 createBoard f = go f 0 0 []
--- test/Main.purs
+++ test/Main.purs
@@ -1,13 +1,8 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE)
-import Node.FS (FS)
-import Node.Process (PROCESS)
-import Test.Spec.Discovery (discover)
-import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (run)
 
-main :: Eff (fs :: FS, process :: PROCESS, console :: CONSOLE) Unit
-main = discover "Sudoku\\..*Spec" >>= run [consoleReporter]
+import Control.Monad.Eff (Eff)
+
+main :: Eff () Unit
+main = pure unit
