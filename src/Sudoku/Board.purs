690e098c5bfc52d5c79d3bd8b880f3b412c3a48a
Jun <me@noraesae.net>
2017-01-11 20:07:01 +0900

    Add some methods to Board

--- src/Sudoku/Board.purs
+++ src/Sudoku/Board.purs
@@ -1,4 +1,45 @@
 module Sudoku.Board where
 
-board :: Int
-board = 1 -- no meaning
+import Prelude
+import Data.Array (snoc, filter)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Eq (genericEq)
+import Data.Generic.Rep.Show (genericShow)
+
+newtype Cell a = Cell { row :: Int, col :: Int, val :: a }
+
+cell :: forall a. Int -> Int -> a -> Cell a
+cell row col val = Cell { row, col, val }
+
+row :: forall a. Cell a -> Int
+row (Cell x) = x.row
+
+col :: forall a. Cell a -> Int
+col (Cell x) = x.col
+
+derive instance genericCell :: Generic (Cell a) _
+
+instance showCell :: Show a => Show (Cell a) where show = genericShow
+instance eqCell :: Eq a => Eq (Cell a) where eq = genericEq
+
+newtype Board a = Board (Array (Cell a))
+
+derive instance genericBoard :: Generic (Board a) _
+
+instance showBoard :: Show a => Show (Board a) where show = genericShow
+
+instance eqBoard :: Eq a => Eq (Board a) where eq = genericEq
+
+createBoard :: forall a. (Board a -> a) -> Board a
+createBoard f = go f 0 0 []
+  where
+    go f row 9 arr = go f (row + 1) 0 arr
+    go _ 9 _ arr = Board arr
+    go f row col arr = go f row (col + 1)
+                       (snoc arr (cell row col (f (Board arr))))
+
+nthRow :: forall a. Board a -> Int -> Array (Cell a)
+nthRow (Board arr) i = filter (row >>> eq i) arr
+
+nthCol :: forall a. Board a -> Int -> Array (Cell a)
+nthCol (Board arr) i = filter (col >>> eq i) arr
--- test/Sudoku/BoardSpec.purs
+++ test/Sudoku/BoardSpec.purs
@@ -5,10 +5,22 @@ import Prelude
 import Test.Spec
 import Test.Spec.Assertions (shouldEqual)
 
+import Data.Array (range)
+
 import Sudoku.Board
 
 spec :: forall r. Spec r Unit
-spec =
-  describe "a test test" do
-    it "test returns 1" do
-      board `shouldEqual` 1
+spec = do
+  let board = Board $ map (\i -> cell (i `div` 9) (i `mod` 9) unit) (range 0 80)
+
+  describe "createBoard" do
+    it "create board" do
+      createBoard (\_ -> unit) `shouldEqual` board
+
+  describe "nthRow" do
+    it "gets the nth row" do
+      nthRow board 3 `shouldEqual` map (\i -> cell 3 i unit) (range 0 8)
+
+  describe "nthCol" do
+    it "gets the nth col" do
+      nthCol board 5 `shouldEqual` map (\i -> cell i 5 unit) (range 0 8)
