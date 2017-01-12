c76b5eca55516553482fba90ce4e99c85c6d20cc
Jun <me@noraesae.net>
2017-01-12 16:24:37 +0900

    Make Cell and Board have a type variable
    It's efficient to make answer and question types clear.

--- src/Sudoku.purs
+++ src/Sudoku.purs
@@ -5,41 +5,50 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Random (RANDOM, randomInt)
 import Data.Array (filter, foldMap, length, range, snoc, updateAt, (!!), (\\))
 import Data.Maybe (Maybe(..), fromJust, isJust)
-import Data.Tuple (Tuple(..))
 import Partial.Unsafe (unsafePartial)
 
-data Cell = Cell Int (Maybe Int)
+fromJust' :: forall a. Maybe a -> a
+fromJust' = unsafePartial fromJust
+
+data Cell a = Cell Int a
+
+derive instance functorCell :: Functor Cell
 
 newtype Row = Row Int
 newtype Col = Col Int
 derive instance rowEq :: Eq Row
 derive instance colEq :: Eq Col
 
-rowOf :: Cell -> Row
+rowOf :: forall a. Cell a -> Row
 rowOf (Cell i _) = Row $ i `div` 9
 
-colOf :: Cell -> Col
+colOf :: forall a. Cell a -> Col
 colOf (Cell i _) = Col $ i `mod` 9
 
-valueOf :: Cell -> Maybe Int
+valueOf :: forall a. Cell a -> a
 valueOf (Cell _ x) = x
 
-instance showCell :: Show Cell where
+instance showCellMaybeInt :: Show (Cell (Maybe Int)) where
   show (Cell _ (Just x)) = show x
   show (Cell _ Nothing) = "x"
 
-newtype Board = Board (Array Cell)
+instance showCellInt :: Show (Cell Int) where
+  show (Cell _ x) = show x
 
-instance showBoard :: Show Board where
+newtype Board a = Board (Array (Cell a))
+
+derive instance functorBoard :: Functor Board
+
+instance showBoard :: (Show (Cell a)) => Show (Board a) where
   show board = foldMap (Row >>> flip rowCells board >>> foldMap show >>> (<>) "\n") (range 0 8)
 
-emptyBoard :: Board
+emptyBoard :: Board (Maybe Int)
 emptyBoard = Board $ map (\i -> Cell i Nothing) (range 0 80)
 
-rowCells :: Row -> Board -> Array Cell
+rowCells :: forall a. Row -> Board a -> Array (Cell a)
 rowCells i (Board arr) = filter (rowOf >>> eq i) arr
 
-colCells :: Col -> Board -> Array Cell
+colCells :: forall a. Col -> Board a -> Array (Cell a)
 colCells i (Board arr) = filter (colOf >>> eq i) arr
 
 data Section = Section Int Int -- Section 0 0 ~ Section 2 2
@@ -47,7 +56,7 @@ data Section = Section Int Int -- Section 0 0 ~ Section 2 2
 sectionOf :: Row -> Col -> Section
 sectionOf (Row r) (Col c) = Section (r `div` 3) (c `div` 3)
 
-contains :: Section -> Cell -> Boolean
+contains :: forall a. Section -> Cell a -> Boolean
 contains (Section secRow secCol) cell =
   let
     rowIdx = case rowOf cell of Row i -> i
@@ -56,35 +65,35 @@ contains (Section secRow secCol) cell =
    rowIdx >= secRow * 3 && rowIdx < (secRow + 1) * 3 &&
    colIdx >= secCol * 3 && colIdx < (secCol + 1) * 3
 
-sectionCells :: Section -> Board -> Array Cell
+sectionCells :: forall a. Section -> Board a -> Array (Cell a)
 sectionCells sec (Board arr) = filter (contains sec) arr
 
 rawIdx :: Row -> Col -> Int
 rawIdx (Row r) (Col c) = r * 9 + c
 
-valueAt :: Row -> Col -> Board -> Maybe Int
-valueAt row col (Board arr) = arr !! rawIdx row col >>= valueOf
+valueAt :: forall a. Row -> Col -> Board a -> a
+valueAt row col (Board arr) = valueOf $ fromJust' (arr !! rawIdx row col)
 
 choose :: forall e. Array Int -> Eff (random :: RANDOM | e) Int
 choose [x] = pure x
 choose xs = do
   idx <- randomInt 0 (length xs - 1)
-  pure $ unsafePartial fromJust $ xs !! idx
+  pure $ fromJust' $ xs !! idx
 
-availableValues :: Row -> Col -> Board -> Array Int
+availableValues :: Row -> Col -> Board (Maybe Int) -> Array Int
 availableValues row col board = range 1 9 \\ already
   where
-    already = map (unsafePartial fromJust <<< valueOf)
+    already = map (fromJust' <<< valueOf)
             $ filter (isJust <<< valueOf)
             $ rowCells row board <> colCells col board <>
               sectionCells (sectionOf row col) board
 
-replaceValue :: Row -> Col -> Maybe Int -> Board -> Board
+replaceValue :: forall a. Row -> Col -> a -> Board a -> Board a
 replaceValue row col val (Board arr) =
-  Board $ unsafePartial fromJust $ updateAt idx (Cell idx val) arr
+  Board $ fromJust' $ updateAt idx (Cell idx val) arr
   where idx = rawIdx row col
 
-emptyRandomCell :: forall e. Board -> Eff (random :: RANDOM | e) Board
+emptyRandomCell :: forall e. Board (Maybe Int) -> Eff (random :: RANDOM | e) (Board (Maybe Int))
 emptyRandomCell board = do
   row <- Row <$> randomInt 0 8
   col <- Col <$> randomInt 0 8
@@ -92,12 +101,12 @@ emptyRandomCell board = do
     Nothing -> emptyRandomCell board -- try again, not smart but works
     Just _ -> pure $ replaceValue row col Nothing board
 
-fullBoard :: forall e. Eff (random :: RANDOM | e) Board
-fullBoard = unsafePartial fromJust <$> go (Row 0) (Col 0) [] emptyBoard
+fullBoard :: forall e. Eff (random :: RANDOM | e) (Board Int)
+fullBoard = fromJust' <$> go (Row 0) (Col 0) [] emptyBoard
   where
-    go :: Row -> Col -> Array Int -> Board -> Eff (random :: RANDOM | e) (Maybe Board)
+    go :: Row -> Col -> Array Int -> Board (Maybe Int)-> Eff (random :: RANDOM | e) (Maybe (Board Int))
     go (Row r) (Col 9) _ board = go (Row $ r + 1) (Col 0) [] board
-    go (Row 9) _ _ board = pure $ Just board
+    go (Row 9) _ _ board = pure <<< Just $ fromJust' <$> board
     go row col@(Col c) alreadyTried board =
       case availableValues row col board \\ alreadyTried of
         [] -> pure Nothing
@@ -109,12 +118,12 @@ fullBoard = unsafePartial fromJust <$> go (Row 0) (Col 0) [] emptyBoard
             Nothing -> go row col (snoc alreadyTried curVal) board
             justBoard -> pure justBoard
 
-solve :: Board -> Array Board
+solve :: Board (Maybe Int) -> Array (Board Int)
 solve = go (Row 0) (Col 0)
   where
-    go :: Row -> Col -> Board -> Array Board
+    go :: Row -> Col -> Board (Maybe Int) -> Array (Board Int)
     go (Row r) (Col 9) board = go (Row $ r + 1) (Col 0) board
-    go (Row 9) _ board = [board]
+    go (Row 9) _ board = [fromJust' <$> board]
     go row col@(Col c) board =
       case valueAt row col board of
         Just _ -> go row (Col $ c + 1) board
@@ -123,15 +132,25 @@ solve = go (Row 0) (Col 0)
 
 type Difficulty = Int
 
-generateGame :: forall e. Difficulty -> Eff (random :: RANDOM | e) (Tuple Difficulty Board)
-generateGame minDify = fullBoard >>= go 0
+data Game = Game Difficulty (Board (Maybe Int)) (Board Int)
+
+instance showGame :: Show Game where
+  show (Game dify ques answ) =
+    "Difficulty: " <> show dify <> "\n\n" <>
+    "Board:" <> show ques <> "\n\n" <>
+    "Answer:" <> show answ
+
+generateGame :: forall e. Difficulty -> Eff (random :: RANDOM | e) Game
+generateGame minDify = do
+  answer <- fullBoard
+  go 0 answer (Just <$> answer)
   where
-    go :: Difficulty -> Board -> Eff (random :: RANDOM | e) (Tuple Difficulty Board)
-    go dify old = do
+    go :: Difficulty -> Board Int -> Board (Maybe Int) -> Eff (random :: RANDOM | e) Game
+    go dify answer old = do
       new <- emptyRandomCell old
       case length $ solve new of
-        0 -> go dify old -- try empty again
-        1 -> go (dify + 1) new -- try more
+        0 -> go dify answer old -- try empty again
+        1 -> go (dify + 1) answer new -- try more
         _ -> if dify > minDify
-            then pure $ Tuple dify old -- return the game
+            then pure $ Game dify old answer -- return the game
             else generateGame minDify -- try from scratch! :sad:
