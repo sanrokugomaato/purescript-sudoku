ff47e3cb77d8f3148b784cf5916e88e9041fd690
Jun <me@noraesae.net>
2017-01-12 15:49:19 +0900

    Make row and col type-safe and remove warnings
    Refactoring.

--- src/Sudoku.purs
+++ src/Sudoku.purs
@@ -3,25 +3,26 @@ module Sudoku where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Random (RANDOM, randomInt)
-import Data.Array (filter, foldMap, foldRecM, length, range, snoc, updateAt, (!!), (\\))
-import Data.Array.Partial (head)
+import Data.Array (filter, foldMap, length, range, snoc, updateAt, (!!), (\\))
 import Data.Maybe (Maybe(..), fromJust, isJust)
 import Data.Tuple (Tuple(..))
 import Partial.Unsafe (unsafePartial)
 
 data Cell = Cell Int (Maybe Int)
 
-type Row = Int
-type Col = Int
+newtype Row = Row Int
+newtype Col = Col Int
+derive instance rowEq :: Eq Row
+derive instance colEq :: Eq Col
 
-row :: Cell -> Row
-row (Cell i _) = i `div` 9
+rowOf :: Cell -> Row
+rowOf (Cell i _) = Row $ i `div` 9
 
-col :: Cell -> Col
-col (Cell i _) = i `mod` 9
+colOf :: Cell -> Col
+colOf (Cell i _) = Col $ i `mod` 9
 
-value :: Cell -> Maybe Int
-value (Cell _ x) = x
+valueOf :: Cell -> Maybe Int
+valueOf (Cell _ x) = x
 
 instance showCell :: Show Cell where
   show (Cell _ (Just x)) = show x
@@ -30,29 +31,39 @@ instance showCell :: Show Cell where
 newtype Board = Board (Array Cell)
 
 instance showBoard :: Show Board where
-  show board = foldMap (flip rowAt board >>> foldMap show >>> (<>) "\n") (range 0 8)
+  show board = foldMap (Row >>> flip rowCells board >>> foldMap show >>> (<>) "\n") (range 0 8)
 
 emptyBoard :: Board
 emptyBoard = Board $ map (\i -> Cell i Nothing) (range 0 80)
 
-rowAt :: Row -> Board -> Array Cell
-rowAt i (Board arr) = filter (row >>> eq i) arr
+rowCells :: Row -> Board -> Array Cell
+rowCells i (Board arr) = filter (rowOf >>> eq i) arr
 
-colAt :: Col -> Board -> Array Cell
-colAt i (Board arr) = filter (col >>> eq i) arr
+colCells :: Col -> Board -> Array Cell
+colCells i (Board arr) = filter (colOf >>> eq i) arr
 
-sectionAt :: Row -> Col -> Board -> Array Cell
-sectionAt secRow secCol (Board arr) = filter f arr
-  where
-    f cell =
-      let r = row cell
-          c = col cell
-      in
-       r >= secRow * 3 && r < (secRow + 1) * 3 &&
-       c >= secCol * 3 && c < (secCol + 1) * 3
+data Section = Section Int Int -- Section 0 0 ~ Section 2 2
+
+sectionOf :: Row -> Col -> Section
+sectionOf (Row r) (Col c) = Section (r `div` 3) (c `div` 3)
+
+contains :: Section -> Cell -> Boolean
+contains (Section secRow secCol) cell =
+  let
+    rowIdx = case rowOf cell of Row i -> i
+    colIdx = case colOf cell of Col i -> i
+  in
+   rowIdx >= secRow * 3 && rowIdx < (secRow + 1) * 3 &&
+   colIdx >= secCol * 3 && colIdx < (secCol + 1) * 3
+
+sectionCells :: Section -> Board -> Array Cell
+sectionCells sec (Board arr) = filter (contains sec) arr
+
+rawIdx :: Row -> Col -> Int
+rawIdx (Row r) (Col c) = r * 9 + c
 
 valueAt :: Row -> Col -> Board -> Maybe Int
-valueAt row col (Board arr) = arr !! (row * 9 + col) >>= value
+valueAt row col (Board arr) = arr !! rawIdx row col >>= valueOf
 
 choose :: forall e. Array Int -> Eff (random :: RANDOM | e) Int
 choose [x] = pure x
@@ -63,64 +74,64 @@ choose xs = do
 availableValues :: Row -> Col -> Board -> Array Int
 availableValues row col board = range 1 9 \\ already
   where
-    already = map (unsafePartial fromJust <<< value)
-            $ filter (isJust <<< value)
-            $ rowAt row board <> colAt col board <>
-              sectionAt (row `div` 3) (col `div` 3) board
+    already = map (unsafePartial fromJust <<< valueOf)
+            $ filter (isJust <<< valueOf)
+            $ rowCells row board <> colCells col board <>
+              sectionCells (sectionOf row col) board
 
 replaceValue :: Row -> Col -> Maybe Int -> Board -> Board
 replaceValue row col val (Board arr) =
   Board $ unsafePartial fromJust $ updateAt idx (Cell idx val) arr
-  where idx = row * 9 + col
+  where idx = rawIdx row col
 
 emptyRandomCell :: forall e. Board -> Eff (random :: RANDOM | e) Board
 emptyRandomCell board = do
-  row <- randomInt 0 8
-  col <- randomInt 0 8
+  row <- Row <$> randomInt 0 8
+  col <- Col <$> randomInt 0 8
   case valueAt row col board of
     Nothing -> emptyRandomCell board -- try again, not smart but works
     Just _ -> pure $ replaceValue row col Nothing board
 
 fullBoard :: forall e. Eff (random :: RANDOM | e) Board
-fullBoard = unsafePartial fromJust <$> go 0 0 [] emptyBoard
+fullBoard = unsafePartial fromJust <$> go (Row 0) (Col 0) [] emptyBoard
   where
-    go :: forall e. Row -> Col -> Array Int -> Board -> Eff (random :: RANDOM | e) (Maybe Board)
-    go row 9 _ board = go (row + 1) 0 [] board
-    go 9 _ _ board = pure $ Just board
-    go row col alreadyTried board =
+    go :: Row -> Col -> Array Int -> Board -> Eff (random :: RANDOM | e) (Maybe Board)
+    go (Row r) (Col 9) _ board = go (Row $ r + 1) (Col 0) [] board
+    go (Row 9) _ _ board = pure $ Just board
+    go row col@(Col c) alreadyTried board =
       case availableValues row col board \\ alreadyTried of
         [] -> pure Nothing
         xs -> do
           curVal <- choose xs
           let curBoard = replaceValue row col (Just curVal) board
-          nextBoard <- go row (col + 1) [] curBoard
+          nextBoard <- go row (Col $ c + 1) [] curBoard
           case nextBoard of
             Nothing -> go row col (snoc alreadyTried curVal) board
             justBoard -> pure justBoard
 
 solve :: Board -> Array Board
-solve board = go 0 0 board
+solve = go (Row 0) (Col 0)
   where
     go :: Row -> Col -> Board -> Array Board
-    go row 9 board = go (row + 1) 0 board
-    go 9 _ board = [board]
-    go row col board =
+    go (Row r) (Col 9) board = go (Row $ r + 1) (Col 0) board
+    go (Row 9) _ board = [board]
+    go row col@(Col c) board =
       case valueAt row col board of
-        Just _ -> go row (col + 1) board
+        Just _ -> go row (Col $ c + 1) board
         Nothing -> flip foldMap (availableValues row col board) \val ->
-          go row (col + 1) $ replaceValue row col (Just val) board
+          go row (Col $ c + 1) $ replaceValue row col (Just val) board
 
 type Difficulty = Int
 
 generateGame :: forall e. Difficulty -> Eff (random :: RANDOM | e) (Tuple Difficulty Board)
 generateGame minDify = fullBoard >>= go 0
   where
-  go :: forall e. Difficulty -> Board -> Eff (random :: RANDOM | e) (Tuple Difficulty Board)
-  go dify old = do
-    new <- emptyRandomCell old
-    case length $ solve new of
-      0 -> go dify old -- try empty again
-      1 -> go (dify + 1) new -- try more
-      _ -> if dify > minDify
-           then pure $ Tuple dify old -- return the game
-           else generateGame minDify -- try from scratch! :sad:
+    go :: Difficulty -> Board -> Eff (random :: RANDOM | e) (Tuple Difficulty Board)
+    go dify old = do
+      new <- emptyRandomCell old
+      case length $ solve new of
+        0 -> go dify old -- try empty again
+        1 -> go (dify + 1) new -- try more
+        _ -> if dify > minDify
+            then pure $ Tuple dify old -- return the game
+            else generateGame minDify -- try from scratch! :sad:
